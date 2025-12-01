683f75b319e789e760a241ddc88edad24300502f
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2025-12-01 11:22:05 +0530

    Update student ID in request_seed.py

--- request_seed.py
+++ request_seed.py
@@ -5,7 +5,7 @@ with open('student_public.pem', 'r') as f:
     public_key = f.read()
 
 # ‚ö†Ô∏è CHANGE THIS TO YOUR STUDENT ID FROM PARTNR
-student_id = "kandala_madhava"  # Replace with YOUR student ID
+student_id = "23P31A1205"  # Replace with YOUR student ID
 github_repo_url = "https://github.com/MadhavaKandala/pki-2fa-microservice"
 
 payload = {
@@ -14,7 +14,7 @@ payload = {
     "public_key": public_key
 }
 
-print("Ì¥Ñ Requesting encrypted seed from instructor API...")
+print("ÔøΩÔøΩÔøΩ Requesting encrypted seed from instructor API...")
 response = requests.post(
     "https://eajeyq4r3zljoq4rpovy2nthda0vtjqf.lambda-url.ap-south-1.on.aws/",
     json=payload,
